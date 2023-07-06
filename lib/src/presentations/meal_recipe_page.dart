51627875b951c8f4894ae7abe17fc97276a2c790
Madalin Tilmaciu <tilmaciu83@gmail.com>
2023-07-06 12:37:11 +0300

    add ingredients list (wip)

--- lib/src/presentations/meal_recipe_page.dart
+++ lib/src/presentations/meal_recipe_page.dart
@@ -4,11 +4,18 @@ import 'package:flutter/material.dart';
 import '../models/index.dart';
 import 'containers/index.dart';
 
-class MealRecipePage extends StatelessWidget {
+class MealRecipePage extends StatefulWidget {
   const MealRecipePage({super.key});
 
   @override
+  State<MealRecipePage> createState() => _MealRecipePageState();
+}
+
+class _MealRecipePageState extends State<MealRecipePage> with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
+    final MediaQueryData mediaQueryData = MediaQuery.of(context);
+
     return SelectedMealContainer(
       builder: (BuildContext context, Meal meal) {
         return RecipeContainer(
@@ -31,7 +38,7 @@ class MealRecipePage extends StatelessWidget {
                                 ),
                               ),
                               width: double.infinity,
-                              height: MediaQuery.of(context).size.height / 2.50,
+                              height: mediaQueryData.size.height / 2.50,
                               child: ClipRRect(
                                 child: CachedNetworkImage(
                                   imageUrl: recipe!.pictureUrl,
@@ -64,11 +71,11 @@ class MealRecipePage extends StatelessWidget {
                       ],
                     ),
                     Positioned(
-                      top: MediaQuery.of(context).size.height / 2.75,
+                      top: mediaQueryData.size.height / 2.75,
                       child: Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 20),
                         child: Container(
-                          width: MediaQuery.of(context).size.width - 40,
+                          width: mediaQueryData.size.width - 40,
                           height: 70,
                           decoration: BoxDecoration(
                             color: Colors.grey[800],
@@ -123,6 +130,263 @@ class MealRecipePage extends StatelessWidget {
                                     color: Colors.red,
                                     size: 32,
                                   ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: mediaQueryData.size.height / 2.25,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                          top: 12,
+                        ),
+                        child: Container(
+                          width: mediaQueryData.size.width - 40,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[800],
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(16),
+                            ),
+                          ),
+                          child: ExpansionTile(
+                            shape: const Border(),
+                            tilePadding: const EdgeInsets.only(
+                              left: 12,
+                              right: 12,
+                            ),
+                            initiallyExpanded: true,
+                            leading: const Image(
+                              height: 25,
+                              color: Colors.white,
+                              image: AssetImage('assets/icons/icons8-ingredients-50.png'),
+                            ),
+                            trailing: const Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Colors.white,
+                            ),
+                            title: Transform.translate(
+                              offset: const Offset(-16, 0),
+                              child: const Text(
+                                'Ingredients',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            children: <Widget>[
+                              const Divider(
+                                color: Colors.white,
+                                height: 5,
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(
+                                recipe.ingredient1!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
                                 ),
                               )
                             ],
